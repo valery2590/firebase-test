399a0e211d378c1843f0883d1db063f9ed012fe5
Valery <valeryfigueroah@gmail.com>
2023-04-17 00:38:02 +0200

    db added

--- src/App.js
+++ src/App.js
@@ -1,56 +1,83 @@
 import React from 'react';
 import './App.css';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, setDoc, doc , addDoc, getFirestore} from "firebase/firestore";
 import { useEffect, useState } from 'react';
 import db from './firebase'
 
 
+
 function App() {
+
+  const initialValues = {
+    age:"",
+    name: "",
+    race:"",
+  }
+
   useEffect(()=>{
     const obtenerDatos = async ()=>{
       const datos = await getDocs(collection(db, 'pets'))
-      console.log(datos.docs[0].data())
+      console.log(datos.docs())
     }
     obtenerDatos();
-    setDato('prueba');
   },[])
 
-  const [dato, setDato] = useState('');
+  const [dato, setDato] = useState(initialValues);
   const [inputName, setInputName] = useState('');
   const [inputAge, setInputAge] = useState('');
   const [inputRace, setInputRace] = useState('');
 
   const handleInputChange = e =>{
-    console.log(e.target.value)
     const {name, value} = e.target;
-    console.log(name, value)
+    setDato({...dato, [name]:value})
   }
 
-  const handleChangeName = e =>{
-    console.log(e.target.value)
-    setInputName(e.target.value)
+  // Add a new doc into the collection
+  const addOrEdit = async () =>{
+   await addDoc(collection(db, "pets"), {
+    age: dato.age,
+    name: dato.name,
+    race: dato.race
+  });
   }
-  const handleChangeAge = e =>{
-    console.log(e.target.value)
-    setInputAge(e.target.value)
-  }
-  const handleChangeRace = e =>{
-    console.log(e.target.value)
-    setInputRace(e.target.value)
+
+  // Get all the docs from a collection
+//   const database = getFirestore();
+//   const colRef = collection(db, "pets");
+//   const docsSnap =  getDocs(colRef);
+//   docsSnap.forEach(doc => {
+//     console.log(doc.data());
+// })
+
+  const handleSubmit = e=>{
+    e.preventDefault();
+    addOrEdit(dato);
+    console.log('new pet added')
+    console.log(dato)
+    setDato({
+      age:"",
+      name:"",
+      race:"",
+    })
   }
 
 return (
     <div>
       <h1>Register your buddy! 🐕🦮</h1>
+      <form onSubmit={handleSubmit}>
       <div className='input-container'>
+      <label>Age of the pet</label>
+        <input type="text" onChange={handleInputChange} value={dato.age} name="age"/>
       <label>Name of the pet</label>
-        <input type="text"  onChange={handleInputChange} name="name"/>
-        <label>Age of the pet</label>
-        <input type="text" onChange={handleInputChange} name="age"/>
+        <input type="text"  onChange={handleInputChange} value={dato.name} name="name"/>
         <label>Race of the pet</label>
-        <input type="text"  onChange={handleInputChange} name="race" />
-        <button type="submit">Add their race</button>
+        <input type="text"  onChange={handleInputChange} value={dato.race} name="race" />
+        <button type="submit" onSubmit={handleSubmit} >Add their race</button>
       </div>
+      </form>
+  <ul>
+
+  </ul>
       
 
     </div>
